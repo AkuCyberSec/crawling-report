89c1949b696eda3bbe9b464bb2e1903d44645e74
Alessandro <alessandro.mazzotti@bcyber.ch>
2025-02-18 15:58:37 +0100

    Added run_no_cache.sh for running and ignoring caches

---
+++ run_no_cache.sh
@@ -0,0 +1,9 @@
+# Modify these volumes:
+# -v "$(pwd)/sample_input":"/app/in"
+# -v "$(pwd)/sample_output":"/app/out"
+#
+# Only modify the path on the host (before ":").
+# Put input files in the mapped folder that corresponds to /app/in 
+# Output files will be generated in the mapped folder that corresponds to /app/out
+
+docker run --rm -v "$(pwd)/config.yaml":"/app/config.yaml" -v "$(pwd)/sample_input":"/app/in" -v "$(pwd)/sample_output":"/app/out" crawlingreport:latest create-report-no-cache
